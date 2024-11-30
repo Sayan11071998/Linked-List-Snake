5dafd53d7edb841e0beacc37f0ad02771933311c
Sayan Nandi <sayannandi11071998@gmail.com>
2024-12-01 00:54:22 +0530

    Assignment - 📝 Starting off with the Level

---
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Level
+{
+	class LevelController
+	{
+	public:
+		LevelController();
+		~LevelController();
+
+		void initialize();
+		void update();
+		void render();
+
+	private:
+	};
+}
---
+++ Linked-List-Snake/include/Level/LevelModel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Level
+{
+	class LevelModel
+	{
+	public:
+		LevelModel();
+		~LevelModel();
+
+	private:
+	};
+}
