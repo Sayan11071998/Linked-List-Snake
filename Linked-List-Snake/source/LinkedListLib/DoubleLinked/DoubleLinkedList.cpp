d8a0d8eba8212844d3154498001c5122558aa12a
Sayan Nandi <sayannandi11071998@gmail.com>
2024-12-12 00:25:28 +0530

    Assignment - 📝Insert at Beginning - DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -8,9 +8,85 @@ namespace LinkedListLib
 {
 	namespace DoubleLinked
 	{
+		Node* DoubleLinkedList::createNode()
+		{
+			return new DoubleNode();
+		}
+
 		DoubleLinkedList::DoubleLinkedList() = default;
 
 		DoubleLinkedList::~DoubleLinkedList() = default;
 
+		void DoubleLinkedList::insertNodeAtTail()
+		{
+		}
+
+		void DoubleLinkedList::insertNodeAtHead()
+		{
+			linked_list_size++;
+			Node* new_node = createNode();
+
+			if (head_node == nullptr)
+			{
+				head_node = new_node;
+				static_cast<DoubleNode*>(new_node)->previous = nullptr;
+				initializeNode(new_node, nullptr, Operation::HEAD);
+				return;
+			}
+
+			initializeNode(new_node, head_node, Operation::HEAD);
+			new_node->next = head_node;
+			static_cast<DoubleNode*>(head_node)->previous = new_node;
+			head_node = new_node;
+		}
+
+		void DoubleLinkedList::insertNodeAtMiddle()
+		{
+		}
+
+		void DoubleLinkedList::insertNodeAtIndex(int index)
+		{
+		}
+
+		void DoubleLinkedList::shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node)
+		{
+		}
+
+		void DoubleLinkedList::removeNodeAtTail()
+		{
+		}
+
+		void DoubleLinkedList::removeNodeAtHead()
+		{
+		}
+
+		void DoubleLinkedList::removeNodeAtMiddle()
+		{
+		}
+
+		void DoubleLinkedList::removeNodeAt(int index)
+		{
+		}
+
+		void DoubleLinkedList::removeNodeAtIndex(int index)
+		{
+		}
+
+		void DoubleLinkedList::removeAllNodes()
+		{
+		}
+
+		void DoubleLinkedList::removeHalfNodes()
+		{
+		}
+
+		void DoubleLinkedList::shiftNodesAfterRemoval(Node* cur_node)
+		{
+		}
+
+		Direction DoubleLinkedList::reverse()
+		{
+			return Direction();
+		}
 	}
 }
