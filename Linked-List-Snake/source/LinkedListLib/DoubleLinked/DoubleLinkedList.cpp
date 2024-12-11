bcf5a75bf029d6a70f95202210d65a3c738bae2a
Sayan Nandi <sayannandi11071998@gmail.com>
2024-12-12 00:15:10 +0530

    Assignment - 📝Create Double Linked List

---
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -0,0 +1,16 @@
+#include "LinkedListLib/DoubleLinked/DoubleLinkedList.h"
+#include "LinkedListLib/DoubleLinked/DoubleNode.h"
+#include "Level/LevelView.h"
+#include "Global/Config.h"
+#include <iostream>
+
+namespace LinkedListLib
+{
+	namespace DoubleLinked
+	{
+		DoubleLinkedList::DoubleLinkedList() = default;
+
+		DoubleLinkedList::~DoubleLinkedList() = default;
+
+	}
+}
