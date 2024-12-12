3238de463df098667432aa288312d1375d749ba2
Sayan Nandi <sayannandi11071998@gmail.com>
2024-12-13 00:34:32 +0530

    Code Refactoring

--- Linked-List-Snake/include/UI/UIService.h
+++ Linked-List-Snake/include/UI/UIService.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "UI/MainMenu/MainMenuUIController.h"
 #include "UI/SplashScreen/SplashScreenUIController.h"
-#include "UI/LevelSelection/LevelSelectionUIController.h"
-#include "UI/GameplayUI/GameplayUIController.h"
 #include "Instructions/InstructionsScreenUIController.h"
+#include "UI/LevelSelection/LevelSelectionUIController.h"
+#include "LinkedListUI/LinkedListSelectionUIController.h"
+#include "UI/GameplayUI/GameplayUIController.h"
 #include "UI/Credits/CreditsScreenUIController.h"
 #include "UI/Interface/IUIController.h"
 
@@ -15,6 +16,7 @@ namespace UI
 		SplashScreen::SplashScreenUIController* splash_screen_controller;
 		MainMenu::MainMenuUIController* main_menu_controller;
 		LevelSelection::LevelSelectionUIController* level_selection_ui_controller;
+		LinkedListUI::LinkedListSelectionUIController* linked_list_ui_controller;
 		GameplayUI::GameplayUIController* gameplay_ui_controller;
 		Instructions::InstructionsScreenUIController* instructions_screen_ui_controller;
 		Credits::CreditsScreenUIController* credits_screen_ui_controller;
